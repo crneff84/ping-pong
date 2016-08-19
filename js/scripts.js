e7fb46a345a7700ee1f5bb21d2e4e715c4367be7
Chance Neff <crneff84@gmail.com>
2016-08-19 08:50:49 -0700

    add front end javascript and update html

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//BACKEND
+
+
+
+
+
+//FRONTEND
+$(document).ready(function(){
+  $("#blanks form").submit(function(event) {
+  event.preventDefault();
+  var userInput = parseInt($("#number-input").val());
+  var output =
+  $("#list-output").empty().append("<li>"+ output + "</li>");
+
+  });
+});
